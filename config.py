57a09a4532952edf7b6c404f54c9f9ee422659ce
Georgii Naidanov <gnaidanov@iMac-Georgii.local>
2026-01-09 18:14:06 +0100

    IDK

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 API_TOKEN = os.getenv("API_TOKEN")
 
 # ---------- GAME ----------
+DROP_COOLDOWN = 6 * 60 * 60
 
 RARITIES = {
     "Common":      {"chance": 60, "points": 5,   "currency": 2},

f5122620f535ea79425fb1e826b45bcd98bf8034
gnaidanov <gnaidanov2007@gmail.com>
2026-09-17 15:16:35 +0200

    update bot

--- database.py
+++ database.py
@@ -758,7 +758,7 @@ def is_chat_paid(chat_id: int) -> bool:
             return cur.fetchone() is not None
 
 def has_67_access(user_id: int, chat_id: int) -> bool:
-    return false
+    return is_user_paid(user_id) or is_chat_paid(chat_id)
 
 def add_paid_user(user_id: int):
     """Сохраняет пользователя как оплатившего"""
